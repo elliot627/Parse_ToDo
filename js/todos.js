57a970766f5efff30b328262435ffcb56e268e4e
Elliot Whitehead <elliotwhitehead1@yahoo.com>
2014-03-05 14:59:41 -0700

    updated todo.js

--- js/todos.js
+++ js/todos.js
@@ -8,7 +8,7 @@ $(function() {
 
   // Initialize Parse with your Parse application javascript keys
   Parse.initialize("xhHZrnElEvleJW4SgsZc9FT84I8rePvcluxuCEE3",
-                   "afj9QhVJpedLow22E0w1cesLEwH7zCvkG9AKGsWR");
+                   "afj9QhVJpedLow22E0w1cesLEwH7zCvkG9AKGsWR ");
 
   // Todo Model
   // ----------
